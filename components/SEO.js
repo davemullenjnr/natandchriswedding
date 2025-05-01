ede666d8f2c28e3157ac18921d0aeb57068bc3f1
Dave Mullen Jnr <davemullenjnr@gmail.com>
2025-05-01 23:42:21 +0100

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -4,28 +4,16 @@ import React from 'react';
 export default function SEO({ title, description, keywords, author }) {
   return (
     <Head>
-      <title>{title || 'davemullenjnr.co.uk Portfolio'}</title>
-      <meta name='description' content={description || 'Portfolio website'} />
+      <title>{title || ''}</title>
+      <meta name='description' content={description || ''} />
       {keywords && <meta name='keywords' content={keywords.join(', ')} />}
       {author && <meta name='author' content={author} />}
-      <meta
-        property='og:title'
-        content={title || 'davemullenjnr.co.uk Portfolio'}
-      />
-      <meta
-        property='og:description'
-        content={description || 'Portfolio website'}
-      />
+      <meta property='og:title' content={title || ''} />
+      <meta property='og:description' content={description || ''} />
       <meta property='og:type' content='website' />
       <meta name='twitter:card' content='summary_large_image' />
-      <meta
-        name='twitter:title'
-        content={title || 'davemullenjnr.co.uk Portfolio'}
-      />
-      <meta
-        name='twitter:description'
-        content={description || 'Portfolio website'}
-      />
+      <meta name='twitter:title' content={title || ''} />
+      <meta name='twitter:description' content={description || ''} />
       {/* Add additional meta tags or override these on a per-page basis */}
     </Head>
   );
