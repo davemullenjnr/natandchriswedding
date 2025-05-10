fde87091a35dcb375566dc586a74200f14c21ecb
Dave Mullen Jnr <davemullenjnr@gmail.com>
2025-05-10 13:39:12 +0100

    Update Background.js

--- components/Background.js
+++ components/Background.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 const Background = styled.div`
   background-image: url('/images/textured-background.jpg');
   background-repeat: repeat;
+  background-attachment: fixed;
+  background-position: center;
   padding-bottom: 6rem;
   min-height: 100vh;
 `;
